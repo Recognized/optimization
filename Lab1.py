06a5ae846ea5f6abcbd7c25f4f99228dc5860e6a
vladislav.saifulin <vladislav.saifulin@jetbrains.com>
2020-03-06 23:05:50 +0300

    Methods

--- Lab1.py
+++ Lab1.py
@@ -22,7 +22,7 @@ def dichotomy(a, b, f, eps):
             b = x2
         if fx2 < fx1:
             a = x1
-    return {"answer": (a + b) / 2, "iterations": iterations, "calc": calc}
+    return {"answer": (a + b) / 2, "iterations": iterations, "calc": calc, "name": "dichotomy"}
 
 
 def golden_section(a, b, f, eps):
@@ -52,7 +52,7 @@ def golden_section(a, b, f, eps):
             x2 = a + (sqrt(5) - 1) / 2 * (b - a)
             fx1 = fx2
             fx2 = g(x2)
-    return {"answer": (a + b) / 2, "calc": calc, "iterations": iterations}
+    return {"answer": (a + b) / 2, "calc": calc, "iterations": iterations, "name": "golden_section"}
 
 
 def number_fibonacci(n):
@@ -73,22 +73,84 @@ def fibonacci(a, b, f, eps):
         return f(x)
 
     k = 1
+    b0 = b
+    a0 = a
     x1 = a + number_fibonacci(n - k + 1) / number_fibonacci(n + 2) * (b - a)
     x2 = a + number_fibonacci(n - k + 2) / number_fibonacci(n + 2) * (b - a)
     fx1 = g(x1)
     fx2 = g(x2)
     while k <= n and b - a >= eps:
+        k = k + 1
         if fx1 < fx2:
             b = x2
             x2 = x1
-            x1 = a + number_fibonacci(n - k + 1) / number_fibonacci(n + 2) * (b - a)
+            x1 = a + number_fibonacci(n - k + 1) / number_fibonacci(n + 2) * (b0 - a0)
             fx2 = fx1
             fx1 = g(x1)
-        if fx2 < fx1:
+        elif fx2 < fx1:
             a = x1
             x1 = x2
-            x2 = a + number_fibonacci(n - k + 2) / number_fibonacci(n + 2) * (b - a)
+            x2 = a + number_fibonacci(n - k + 2) / number_fibonacci(n + 2) * (b0 - a0)
             fx1 = fx2
             fx2 = g(x2)
-        k = k + 1
-    return {"answer": (a + b) / 2, "calc": calc, "iterations": iterations}
+        iterations += 1
+    return {"answer": (a + b) / 2, "calc": calc, "iterations": iterations, "name": "fibonacci"}
+
+
+def straight(f, method, eps):
+    a = 0
+    b = 1
+    d = 1
+
+    iterations = 0
+
+    f1 = f(a)
+    f2 = f(b)
+
+    calc = 2
+
+    if f2 > f1:
+        d = -d
+
+    k1 = f1
+    k2 = f2
+
+    while f1 < f2 and k1 <= k2 or f1 > f2 and k1 >= k2:
+        iterations += 1
+        if d < 0:
+            a = a + d
+            f1 = f(a)
+        else:
+            b = b + d
+            f2 = f(b)
+        d = d * 2
+        calc += 1
+
+    m = method(a, b, f, eps)
+    return {"answer": m["answer"], "calc": m["calc"] + calc, "iterations": m["iterations"] + iterations,
+            "name": "straight"}
+
+
+def task_1():
+    def f(x):
+        return 3 * x * x + 5 * x - 6
+
+    eps = [.1, .01, .001, .0001]
+
+    a = -1000
+    b = 1000
+
+    methods = [
+        lambda e: dichotomy(a, b, f, e),
+        lambda e: fibonacci(a, b, f, e),
+        lambda e: golden_section(a, b, f, e),
+        lambda e: straight(f, golden_section, e)
+    ]
+
+    for method in methods:
+        for e in eps:
+            print(method(e))
+
+
+if __name__ == '__main__':
+    task_1()
